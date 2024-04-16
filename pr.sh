7795f003863afc907efbcf37a692c89fcbec216c
NikitaSkrynnik <nikita.skrynnik@xored.com>
2024-04-16 17:21:57 +0700

    fix
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- pr.sh
+++ pr.sh
@@ -1,8 +1,9 @@
-git branch -D pr
-git push fork --delete pr
-git checkout -b pr
+git checkout main
+git branch -D  update/networkservicemesh/pr
+git push fork --delete  update/networkservicemesh/pr
+git checkout -b  update/networkservicemesh/pr
 echo "change" > change
 git add .
 git commit -sm change
-git push -u fork pr
-open -a "Google Chrome" https://github.com/NikitaSkrynnik/cmd-nse-vlan-vpp/pull/new/pr
+git push -u fork update/networkservicemesh/pr
+open -a "Google Chrome" https://github.com/NikitaSkrynnik/cmd-nse-vlan-vpp/pull/new/update/networkservicemesh/pr
